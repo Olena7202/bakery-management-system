7d845bb6cdcb9be8094317b011930de4b6a66545
Viola Stan <violastan22@gmail.com>
2026-05-21 14:26:40 +0300

    fix: change the type in column name table user

--- sweetVenomServer/sweetVenomServer/Models/User.cs
+++ sweetVenomServer/sweetVenomServer/Models/User.cs
@@ -7,6 +7,7 @@ public partial class User
 {
     public int Id { get; set; }
 
+    [Column(TypeName = "nvarchar(100)")]
     public string Name { get; set; } = null!;
 
     public string Email { get; set; } = null!;
